3e54ae38100a706cca81796d3f91472bdd33bbd5
David Kahn <david@mac.mynetworksettings.com>
2026-03-31 07:35:51 -0400

    Bump SW cache to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bookshelf-v2';
+const CACHE='bookshelf-v3';
 const CORE=['/bookshelf/','/bookshelf/index.html','/bookshelf/data.json','/bookshelf/manifest.json'];
 
 self.addEventListener('install',e=>{
