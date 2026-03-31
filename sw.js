22e3cb598f3a76cb441ec4c94db251cb99d6e5ab
David Kahn <david@mac.mynetworksettings.com>
2026-03-31 08:50:50 -0400

    Fix picker z-index for modal, add 3D spine hover, gate recs behind ratings

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bookshelf-v3';
+const CACHE='bookshelf-v4';
 const CORE=['/bookshelf/','/bookshelf/index.html','/bookshelf/data.json','/bookshelf/manifest.json'];
 
 self.addEventListener('install',e=>{
