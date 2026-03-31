5bcfbbd51d9c7cac0945601ed5f37b8bbeaab1cf
David Kahn <david@mac.mynetworksettings.com>
2026-03-31 12:57:02 -0400

    Add 41 Audible audiobooks, bump cache to v13

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bookshelf-v12';
+const CACHE='bookshelf-v13';
 const CORE=['/bookshelf/','/bookshelf/index.html','/bookshelf/data.json','/bookshelf/manifest.json'];
 
 self.addEventListener('install',e=>{
