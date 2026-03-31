d41d508d379e7fd4918bef917fffba2301222125
David Kahn <david@mac.mynetworksettings.com>
2026-03-31 09:10:58 -0400

    Fix status picker position, add Type to stats, single-click Type dropdown in modal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bookshelf-v5';
+const CACHE='bookshelf-v6';
 const CORE=['/bookshelf/','/bookshelf/index.html','/bookshelf/data.json','/bookshelf/manifest.json'];
 
 self.addEventListener('install',e=>{
